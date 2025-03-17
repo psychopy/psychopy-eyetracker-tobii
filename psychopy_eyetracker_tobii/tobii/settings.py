f3ec8bd7cf89b35d90871ccce2422b2d70b981d0
Matthew D. Cutone <mcutone@opensciencetools.org>
2025-03-17 16:53:11 -0400

    NF: Added eyetracker settings

---
+++ psychopy_eyetracker_tobii/tobii/settings.py
@@ -0,0 +1,58 @@
+from psychopy.localization import _translate
+from psychopy.experiment import Param
+from psychopy.experiment.components.settings.eyetracking import EyetrackerBackend
+
+
+class TobiiEyetrackerBackend(EyetrackerBackend):
+    """Experiment settings for Tobii eyetrackers.
+    """
+    label = 'Tobii Technology'
+    key = 'eyetracker.hw.tobii.EyeTracker'
+
+    needsFullscreen = False
+    needsCalibration = False
+
+    @classmethod
+    def getParams(cls):
+        # define order
+        order = [
+            # runtime settings
+            "tbSamplingRate",
+            "tbTrackEyes"
+        ]
+
+        # network settings
+        params = {}
+        # runtime settings
+        params['tbSamplingRate'] = Param(
+            "60", 
+            valType='str', 
+            inputType="single",
+            hint=_translate("Sampling rate in Hz."),
+            label=_translate("Sampling rate"), 
+            categ="Eyetracking"
+        )
+        params['tbTrackEyes'] = Param(
+            'BINOCULAR', 
+            valType='str', 
+            inputType="choice",
+            allowedVals=['BINOCULAR'],
+            hint=_translate("Which eye(s) to track."),
+            label=_translate("Track eyes"), 
+            categ="Eyetracking"
+        )
+
+        return params, order
+
+    @classmethod
+    def writeDeviceCode(cls, inits, buff):
+        code = (
+            "ioConfig[%(eyetracker)s] = {\n"
+            "    'name': 'tracker',\n"
+            "    'runtime_settings': {\n"
+            "        'sampling_rate': %(tbSamplingRate)s,\n"
+            "        'track_eyes': %(tbTrackEyes)s,\n"
+            "    },\n"
+            "}\n"
+        )
+        buff.writeIndentedLines(code % inits)
